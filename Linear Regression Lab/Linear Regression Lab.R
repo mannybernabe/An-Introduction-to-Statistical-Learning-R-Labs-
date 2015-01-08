29774cd29dbd66db568df250650df175232346dd
Manny B! <bernabe.emmanuel@gmail.com>
2015-01-08 06:30:48 -0600

    update

--- Linear Regression Lab/Linear Regression Lab.R
+++ Linear Regression Lab/Linear Regression Lab.R
@@ -29,4 +29,27 @@ predict(lm.fit, newdata=data.frame(lstat=c(5,10,15)),interval="confidence")
 
 
 #with predictive intervals.
-predict(lm.fit, newdata=data.frame(lstat=c(5,10,15)),interval="confidence")
+predict(lm.fit, newdata=data.frame(lstat=c(5,10,15)),interval="prediction")
+#recall that predicative intervals are going to wider than the confidence intervals
+
+
+plot(boston$lstat,boston$medv)
+#visually, we suspect some non-linearity.  We will addrss a bit later. 
+
+#plot line over scatterplott
+abline(lm.fit,lwd=3,col="red")
+
+par(mfrow=c(2,2))
+
+plot(lm.fit)
+
+par(mfrow=c(1,1))
+plot(predict(lm.fit),residuals(lm.fit))
+plot(predict(lm.fit),rstudent(lm.fit))
+
+#compute leverage for points
+plot(hatvalues(lm.fit))
+which.max(hatvalues(lm.fit))
+
+##Working with multiple linear regression
+
